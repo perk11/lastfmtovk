a93cfb7f209b5ba735d0cfd17ec218734a32a9bd
Pereyaslov Konstantin <perk11@perk11.info>
2014-02-24 07:04:52 +0400

    Установочный проект WiX

---
+++ wix/build.bat
@@ -0,0 +1,3 @@
+
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\candle.exe" -ext WiXNetFxExtension -ext WixUIExtension -ext WixUtilExtension lastfmtovk.wxs
+"C:\Program Files (x86)\WiX Toolset v3.8\bin\light.exe" -cultures:"ru-RU,en-us" -ext WiXNetFxExtension -ext WixUIExtension -ext WixUtilExtension lastfmtovk.wixobj
